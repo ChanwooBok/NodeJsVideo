bc99d43a33e69e45aff91dd2fc60cf91ade7c95b
chanwooBok <titoddd@naver.com>
2022-04-03 21:20:05 +0900

    Create upload & edit & search Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -20,35 +20,43 @@ import Video from "../models/Video";
 //promise 방식 ( 코드가 순서대로 실행되서 가독성이 좋다 . )
 export const home = async(req,res) => {
   try{
-    console.log("start");
-    const videos = await Video.find( {});
-    console.log(videos);
-    console.log("finished");
+    const videos = await Video.find( {}).sort({createdAt : "desc" });
     return res.render("home", { pageTitle :  "Home" , videos });
   }catch(error){
     return res.render("server-error");
   }
 }
 
-export const watch = (req, res) => {
+export const watch = async(req, res) => {
   const { id } = req.params; // const id = req.param.id
-  const video = videos[id - 1];
-  
-  return res.render("watch", { pageTitle: `Watching hell yeah `});
+  const video = await Video.findById(id);
+  if(!video){
+    return res.render("404", {pageTitle: "Video Not Found"});
+  }
+  return res.render("watch", { pageTitle: `Watching hell yeah ` , video });
+};
+ 
+export const getEdit = async(req,res)=> {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if(!video){
+    return res.render("404", {pageTitle: "Video Not Found"});
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
-export const test = (req,res) => {
-  return res.render("test");
-}
-
-export const getEdit = (req,res)=> {
+export const postEdit  = async(req,res)=>{
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing `  });
-};
-
-export const postEdit  = (req,res)=>{
-  const { id } = req.params;
-  const { title } = req.body;
+  const { title,description , hashtags } = req.body;
+  const video = await Video.exists( { _id : id });
+  if(!video){
+    return res.render("404", {pageTitle: "Video Not Found"});
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags  : Video.formatHashtags(hashtags),
+  });
   return res.redirect(`/videos/${id}`);
 };
 
@@ -57,19 +65,39 @@ export const getUpload = (req,res)=>{
 }
 export const postUpload = async(req,res)=> {
   const {title , description , hashtags } = req.body;
-
-  Video.create( {
+ try{
+   await Video.create( {
     title , 
     description , 
-    createdAt : Date.now(),
-    hashtags : hashtags.split(",").map( ( word ) => `#${word}`),
-    meta:{
-      views: 0,
-      rating: 0,
-    },
+    hashtags  : Video.formatHashtags(hashtags),
   });
+ }catch(error){
+    return res.render("upload", {
+      pageTitle : "Upload Video",
+      errorMessage : error._message,
+    })
+ }
   
   return res.redirect("/");
 }
 
 
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+
+export const search = async(req,res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if(keyword){
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`^${keyword}`, "i"),
+      },
+    })
+  }
+  return res.render("search", { pageTitle: "Search", videos });
+}
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,15 +1,35 @@
 import mongoose from "mongoose";
 
+// mongoose 로 하여금 비디오 스키마 형식을 알려줘서 해당 모델이 해당 형식으로만 저장되게끔 도와주도록 한다.
+// mongoose가 잘못된 정보를 저장하면 데이터 타입의 유효성을 검사해서 알맞지 않으면 저장하지 않도록 도와준다.
+// 예 : meta의 views, rating은 숫자인데 "sdfjasdljfkq" 적으면 meta는 저장되지 않은채 생성된다.  
+
 const videoSchema = new mongoose.Schema({
-    title:String,
-    description : String,
-    createdAt : Date,
-    hashtags:[ {type: String }],
+    title: { type: String, required: true, trim: true, maxLength: 80 },
+    description: { type: String, required: true, trim: true, minLength: 20 },
+    createdAt : {type : Date , required : true , default : Date.now},
+    hashtags: [{ type: String, trim: true }],
     meta:{
-        views:Number,
-        rating:Number,
+        views: { type : Number , default : 0 , required : true},
+        rating: { type : Number , default : 0 , required : true },
     },
 });
+
+
+ /* pre.save 미들웨어를 이용한 방법 */
+// videoSchema.pre("save" , async function(){
+//     this.hashtags = this.hashtags[0]
+//             .split(",")
+//             .map( (word) => (word.startsWith("#") ? word : `#${word}`));
+//   });
+
+/* static을 이용하여 video function을 만드는 방법  */
+videoSchema.static("formatHashtags", function( hashtags ){
+    return hashtags
+            .split(",")
+            .map( (word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
 
 const Video = mongoose.model("Video",videoSchema);
 
