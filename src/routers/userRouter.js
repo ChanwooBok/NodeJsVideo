43dca452e05f5c4ce3aa177743e2c585f24d0b80
chanwooBok <titoddd@naver.com>
2022-05-02 21:03:21 +0900

    create Edit profile

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,7 +4,7 @@ import {protectorMiddleware , publicOnlyMiddleware} from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.router("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/remove", remove);
 userRouter.get("/logout",protectorMiddleware, logout);
 userRouter.get("/github/start" ,publicOnlyMiddleware,  startGithubLogin);
